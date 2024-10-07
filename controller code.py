c6bb96f67fc1cfe296dc4ff17c72f1319212e41e
lswart1 <31469620+lswart1@users.noreply.github.com>
2024-10-07 15:42:32 +0800

    Update controller code.py

--- controller code.py
+++ controller code.py
@@ -18,7 +18,7 @@ joystick.init()
 arm_state = 1500
 
 # ESP32 server URL
-ESP32_URL = "http://<ESP32_IP>/action"
+ESP32_URL = "http://192.168.0.190/action"
 
 def send_values(roll, pitch, yaw, throttle, arm):
     params = {
@@ -39,16 +39,16 @@ try:
         pygame.event.pump()
 
         # Read joystick axes
-        left_stick_x = joystick.get_axis(0)  # X-axis for roll
-        left_stick_y = joystick.get_axis(1)  # Y-axis for pitch
-        right_stick_x = joystick.get_axis(2)  # X-axis for yaw
-        right_stick_y = joystick.get_axis(3)  # Y-axis for throttle (inverted)
+        left_stick_x = joystick.get_axis(0)  # 
+        left_stick_y = joystick.get_axis(1)  # 
+        right_stick_x = joystick.get_axis(2)  # 
+        right_stick_y = joystick.get_axis(3)  # 
 
         # Map joystick values to required ranges
-        roll = int((left_stick_x + 1) * 500 + 1500)  # Roll: 1000 to 2000
-        pitch = int((left_stick_y + 1) * 500 + 1500)  # Pitch: 1000 to 2000
-        yaw = int((right_stick_x + 1) * 500 + 1500)  # Yaw: 1000 to 2000
-        throttle = int((1 - (right_stick_y + 1) / 2) * 1000 + 1000)  # Throttle: 1000 to 2000
+        roll = int((right_stick_x) * 30 + 1500)  # Roll: 1000 to 2000
+        pitch = int((-right_stick_y) * 30 + 1500)  # Pitch: 1000 to 2000
+        yaw = int((left_stick_x) * 30 + 1500)  # Yaw: 1000 to 2000
+        throttle = int(((-left_stick_y) / 2) * 1000 + 1000)  # Throttle: 1000 to 2000
 
         # Check for button press (Button X is usually button 0)
         if joystick.get_button(0):  # Button X
